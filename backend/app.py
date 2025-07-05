e4763585c9e7c55efa5bb063d8c512f6dfe085f1
JuanEspinoH <juanespinoholguin@gmail.com>
2025-07-05 01:43:11 -0600

    ignore env 2

--- backend/app.py
+++ backend/app.py
@@ -30,7 +30,7 @@ class Product(db.Model):
     name = db.Column(db.String(50))
     def __repr__(self):
         return f"<Product {self.name}>"
-    # 1
+    # 11
 class Frutas(db.Model):
     __tablename__ = "frutas"
     id = db.Column(db.Integer, primary_key=True)
