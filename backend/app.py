d7df563607a3c7ed0cec4584776a1ed5c26057b4
JuanEspinoH <juanespinoholguin@gmail.com>
2025-07-05 17:59:38 -0600

    modelos agregados

--- backend/app.py
+++ backend/app.py
@@ -1,70 +1,37 @@
 import os
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
+from models import db,Usuario
 from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager,jwt_required
 
 load_dotenv()
 
 app=Flask(__name__)
-
-USER = os.getenv("user")
-PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
-PASSWORD = os.getenv("password")
-
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+CORS(app)
+jwt = JWTManager() 
+bcrypt = Bcrypt()  
 
 app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv("CONNECTION_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 migrate = Migrate(app, db)
 
-class Product(db.Model):
-    __tablename__ = "products"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    def __repr__(self):
-        return f"<Product {self.name}>"
-    # 111
-class Frutas(db.Model):
-    __tablename__ = "frutas"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    def __repr__(self):
-        return f"<Fruta {self.name}>"
-    
+
 with app.app_context():
     db.create_all()
 
-@app.route("/test")
-def hello_world2():
-    print("test")
-    apple = Frutas(name="Apple")
-    range = Frutas(name="Orange")
-    anana = Frutas(name="Banana")
-    db.session.add_all([apple, range, anana])
-    db.session.commit()
-    print(Frutas.query.all())
-    return "<p>Hello, World!</p>"
 
 @app.route("/")
 def hello_world():
-    print("home")
-    apple = Product(name="Apple")
-    range = Product(name="Orange")
-    anana = Product(name="Banana")
-    db.session.add_all([apple, range, anana])
-    db.session.commit()
-    print(Product.query.all())
-    return "<p>Hello, World!</p>"
-
-
-
+    newUser=Usuario()
+    db.session.add(newUser)
+    db.session.commit()  
+    return jsonify({"test":"pruebas"}),200
 
 
 if __name__ == '__main__':
---
+++ backend/models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, ForeignKey,String
+
+db = SQLAlchemy()
+# id, username, email,
+# password (hashed).
+# id, label, completed, user_id
+
+class Usuario(db.Model):
+    __tablename__="usuarios"
+    id = db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(15),nullable=False)
+    email=db.Column(db.String(20),nullable=False,unique=True)
+    password=db.Column(db.String,nullable=False)
+
+    def serialize(self):
+        return{
+            "id":self.id,
+            "username":self.username,
+            "email":self.email,
+            }
+
+class Tarea(db.Model):
+    __tablename__="tareas"
+    id = db.Column(db.Integer, primary_key=True)
+    label=db.Column(db.String,nullable=False)
+    completed=db.Column(db.Boolean,nullable=False,default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    def serialize(self):
+                return{
+                    "id":self.id,
+                    "label":self.label,
+                    "completed":self.completed,
+                    "user_id":self.user_id,
+                    }
